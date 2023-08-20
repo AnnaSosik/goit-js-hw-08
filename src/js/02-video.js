ea8ce6dc1c8be4f2a620c77433e519ff333dc1a3
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-08-20 20:09:47 +0200

    task 03 updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,16 @@
 import Player from '@vimeo/player'
 import throttle from 'lodash.throttle'
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+}, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const savedTime = localStorage.getItem(STORAGE_KEY);
 
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-updateOutput();
-
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
-  }, 1000)
-);
-
-function updateOutput() {
-  try {
-    const parsedSettings = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    player.setCurrentTime(parsedSettings);
-  } catch {}
+if (savedTime) {
+    player.setCurrentTime(savedTime);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,36 @@
 import throttle from 'lodash.throttle';
 
-const formFeedback = document.querySelector('.feedback-form');
-const inputEmail = formFeedback.querySelector('input');
-const textareaMessage = formFeedback.querySelector('textarea');
-const buttonForm = formFeedback.querySelector('button');
+const feedbackForm = document.querySelector('.feedback-form');
+const inputEmail = feedbackForm.querySelector('input');
+const textareaMessage = feedbackForm.querySelector('textarea');
+const buttonForm = feedbackForm.querySelector('button');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 let newObj = {};
+
+feedbackForm.addEventListener(
+    'input',
+    throttle(() => {
+      newObj.email = inputEmail.value;
+      newObj.message = textareaMessage.value;
+  
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newObj));
+    }, 500)
+  );
+  
+  function updateOutput() {
+    try {
+      const parsedSettings = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+      inputEmail.value = parsedSettings.email || '';
+      textareaMessage.value = parsedSettings.message || '';
+    } catch {}
+  }
+  
+  feedbackForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    inputEmail.value = '';
+    textareaMessage.value = '';
+    console.log(newObj);
+  });
